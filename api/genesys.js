beacb9599d027d0275cb689407f7abc3ddbd8197
Wassac <46289535+Wassac@users.noreply.github.com>
2025-09-19 00:55:52 +0900

    Delete api/genesys.js

--- api/genesys.js
+++
@@ -1,6 +0,0 @@
-export const config = { runtime: "edge" };
-export default async function handler() {
-  return new Response(JSON.stringify({ ok: true, endpoints: ["save","get","list","delete","roll"] }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
