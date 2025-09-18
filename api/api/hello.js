8ca54ec903fe1441f55a552420fd855089979409
Wassac <46289535+Wassac@users.noreply.github.com>
2025-09-19 00:56:27 +0900

    Delete api/api/hello.js

--- api/api/hello.js
+++
@@ -1,6 +0,0 @@
-export const config = { runtime: "edge" };
-export default async function handler() {
-  return new Response(JSON.stringify({ ok: true, msg: "hello" }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
